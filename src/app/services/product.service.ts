2de73dfa137ba97f9c50037b9989b58832d1ca32
Roman Surkov <s.rb@list.ru>
2023-08-08 22:03:45 +0200

    Added product class, service, product list component

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {Product} from "../common/product";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private host = 'localhost';
+  private port = 8080;
+  private baseUrl: string = 'http://' + this.host + ':' + this.port + '/api/products';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
