81235ae6f9412aec8472452e1821fc24c96904a3
Roman Surkov <s.rb@list.ru>
2023-10-04 20:52:50 +0200

    Client's email to payment info

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -184,6 +184,7 @@ export class CheckoutComponent implements OnInit {
     // compute payment info
     this.paymentInfo.amount = Math.round(this.totalPrice * 100);
     this.paymentInfo.currency = "USD";
+    this.paymentInfo.receiptEmail = purchase.customer.email;
 
     console.log(`this.paymentInfo.amount = ${this.paymentInfo.amount}`);
     // if valid form then
