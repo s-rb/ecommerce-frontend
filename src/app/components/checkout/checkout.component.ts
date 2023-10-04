2e6a4548fd8ce6dc1548ad394783bd588c4a6ce6
Roman Surkov <s.rb@list.ru>
2023-10-04 20:35:41 +0200

    Disabling purchase button

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -41,6 +41,8 @@ export class CheckoutComponent implements OnInit {
   cardElement: any;
   displayError: any = "";
 
+  isDisabled: boolean = false;
+
   theEmail: string = '';
 
   constructor(private formBuilder: FormBuilder,
@@ -189,6 +191,9 @@ export class CheckoutComponent implements OnInit {
     // - confirm card payment
     // - place order
     if (!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
+
+      this.isDisabled = true;
+
       this.checkoutService.createPaymentIntent(this.paymentInfo).subscribe(
         (paymentIntentResponse) => {
           this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
@@ -212,6 +217,7 @@ export class CheckoutComponent implements OnInit {
               if (result.error) {
                 // inform the customer there was an error
                 alert(`There was an error: ${result.error.message}`);
+                this.isDisabled = false;
               } else {
                 // call Rest API via the CheckoutService
                 this.checkoutService.placeOrder(purchase).subscribe({
@@ -220,9 +226,11 @@ export class CheckoutComponent implements OnInit {
 
                     // reset cart
                     this.resetCart();
+                    this.isDisabled = false;
                   },
                   error: (err: any) => {
                     alert(`There was an error: ${err.message}`);
+                    this.isDisabled = false;
                   }
                 })
               }
