3ceffa7b1ed2bf09a4d13959e8b213b8ed110078
Roman Surkov <s.rb@list.ru>
2023-10-07 09:28:02 +0200

    Port changed for the backend

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  luv2shopApiUrl: "https://localhost:8443/api",
+  luv2shopApiUrl: "https://localhost:9898/api",
   stripePublishableKey: "pk_test_51NwW14DGMzHkgb75Dn9Fe8spmQkSefEBJSOAxqUZb8DV1SD0RnRrf7RAyM2nUOIovE6A2I6h7JOtmKjYf8IKoKJs00QBJ1N47K"
 };
